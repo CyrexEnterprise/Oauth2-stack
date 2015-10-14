1babb9bc58fb59fd4e8894176dc82dc27fb02950
Cátia Araújo <catiaaraujo@boldint.com>
2015-10-14 20:01:57 +0100

    chaging ServiceProvider boot method in order to migrate package from Laravel 4.2 to Laravel 5.0

--- src/Cloudoki/OaStack/OaStackServiceProvider.php
+++ src/Cloudoki/OaStack/OaStackServiceProvider.php
@@ -1,54 +1,66 @@
-<?php namespace Cloudoki\OaStack;
+<?php
+
+namespace Cloudoki\OaStack;
 
 use Illuminate\Support\ServiceProvider;
 
-class OaStackServiceProvider extends ServiceProvider {
+class OaStackServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('cloudoki/oastack');
-		
-		# Oauth2 Routes
-		include __DIR__.'/../../routes.php';
-		
-		# Ouath2 simple filter
-		include __DIR__.'/../../filters.php';
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+        $this->publishes([
+            __DIR__.'/../../config/config.php' => config_path('cloudoki.php'),
+        ]);
 
-		$this->app['oastack'] = $this->app->share(function($app)
-        {
+        $this->loadViewsFrom(__DIR__ . '/../../views', 'oastack');
+
+        # Oauth2 Routes
+        include __DIR__.'/../../routes.php';
+
+        # Ouath2 simple filter
+        include __DIR__.'/../../filters.php';
+
+        $this->publishes([
+            __DIR__.'/../../migrations/' => database_path('migrations')
+        ], 'migrations');
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->app['oastack'] = $this->app->share(function ($app) {
             return new OaStack;
         });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('oastack');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('oastack');
+    }
 
 }
