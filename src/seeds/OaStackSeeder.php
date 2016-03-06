834ee68b6e3d8bb3bc1439eabe7de89e8838c52b
Koen Betsens <koen.betsens@gmail.com>
2016-03-06 13:31:34 +0100

    minor

--- src/seeds/OaStackSeeder.php
+++ src/seeds/OaStackSeeder.php
@@ -14,36 +14,3 @@ class OaStackSeeder extends Seeder
         $this->command->info('OaStack tables seeded!');
     }
 }
-
-class OaAccountsTableSeeder extends Seeder {
-
-    public function run()
-    {
-       Account::create(['name' => 'Project Account']);
-    }
-
-}
-
-class OaUsersTableSeeder extends Seeder {
-
-    public function run()
-    {
-        User::create(['email' => 'jane@doe.com', 'firstname'=> 'Jane', 'lastname'=> 'Doe']);
-    }
-
-}
-
-class OauthClientsTableSeeder extends Seeder {
-
-    public function run()
-    {
-        $client = new Oauth2Client ();
-		$client ->setUser (1)
-				->setClientId ()
-				->setClientSecret ()
-				->setName ('Project app')
-				->setRedirectUri ('http://localhost/projectapp/auth.html')
-				->save ();
-    }
-
-}
